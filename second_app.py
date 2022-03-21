25caf9199eb4f285de5dcd4626e02818c9691599
samm4230 <102031809+samm4230@users.noreply.github.com>
2022-03-21 15:22:17 +0000

    Update second_app.py

--- second_app.py
+++ second_app.py
@@ -7,3 +7,11 @@ slider = st.slider("This slides", 0, 500)
 
 cities = ['Manchester', 'Tokyo', 'Brasilia', 'Ottawa', 'Wellington', 'Nairobi', 'Berlin']
 st.selectbox('Choose a city', cities)
+
+button1 = st.button('Press for cold')
+if button1:
+  st.snow()
+
+button2 = st.button('Press for helium')
+if button2:
+  st.balloons()
