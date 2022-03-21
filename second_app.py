6ee187f46db012fdf6a3e0a5a2d4c9743f51a130
samm4230 <102031809+samm4230@users.noreply.github.com>
2022-03-21 15:24:01 +0000

    Update second_app.py

--- second_app.py
+++ second_app.py
@@ -3,6 +3,9 @@ import streamlit as st
 st.title("Hello there")
 st.header("A surprise to be sure, but a welcome one.")
 
+first_name = st.text_input('What is your first name?')
+print(f'Hi {first_name} :smile:')
+
 slider = st.slider("This slides", 0, 500)
 
 cities = ['Manchester', 'Tokyo', 'Brasilia', 'Ottawa', 'Wellington', 'Nairobi', 'Berlin']
