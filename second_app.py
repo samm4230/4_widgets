da9117b2550102d1810f9697d8ee5c11dad760f3
samm4230 <102031809+samm4230@users.noreply.github.com>
2022-03-21 15:17:45 +0000

    Update second_app.py

--- second_app.py
+++ second_app.py
@@ -4,3 +4,6 @@ st.title("Hello there")
 st.header("A surprise to be sure, but a welcome one.")
 
 slider = st.slider("This slides", 0, 500)
+
+cities = ['Manchester', 'Tokyo', 'Brasilia', 'Ottawa', 'Wellington', 'Nairobi', 'Berlin']
+st.selectbox('Choose a city', cities)
