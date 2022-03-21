0493181405b757a0767b43e23964238b213b7a6e
samm4230 <102031809+samm4230@users.noreply.github.com>
2022-03-21 15:24:29 +0000

    Update second_app.py

--- second_app.py
+++ second_app.py
@@ -4,7 +4,7 @@ st.title("Hello there")
 st.header("A surprise to be sure, but a welcome one.")
 
 first_name = st.text_input('What is your first name?')
-print(f'Hi {first_name} :smile:')
+f'Hi {first_name} :smile:'
 
 slider = st.slider("This slides", 0, 500)
 
